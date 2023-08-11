b67d3c93b3bead9298f982e776c5eff542e0fa19
Filip Leonarski <filip.leonarski@psi.ch>
2023-08-11 12:12:43 +0200

    Commit 2

--- process.py
+++ process.py
@@ -9,8 +9,15 @@ ponifilename = sys.argv[3]
 
 n_bins_rad = 500
 n_bins_azi = 16
+pixel_split = 2
 
-direct_beam_ROI = (slice(2377, 2389), slice(1990, 2015))
+direct_beam_min_x = 1990
+direct_beam_max_x = 2015
+direct_beam_min_y = 2377
+direct_beam_max_y = 2389
+
+direct_beam_ROI = (slice(direct_beam_min_y, direct_beam_max_y),
+                   slice(direct_beam_min_x, direct_beam_max_x))
 
 with h5py.File(h5filename_in, 'r') as input_file:
     data_container = input_file["/entry/data/data"]
@@ -30,24 +37,27 @@ with h5py.File(h5filename_in, 'r') as input_file:
     az = azint.AzimuthalIntegrator(ponifilename,
                                    shape=image_size,
                                    pixel_size=75.0e-06,
-                                   n_splitting=2,
+                                   n_splitting=pixel_split,
                                    radial_bins=n_bins_rad,
                                    azimuth_bins=n_bins_azi,
                                    unit='q',
                                    mask=bad_pix_map,
                                    solid_angle=True,
+                                   error_model=None,
                                    polarization_factor=None)
-
-    n_pix = az.integrate(bad_pix_map, None, False)[2]
 
     for column_index in range(nimages):
         res = az.integrate(data_container[column_index, :, :])
         output_data[column_index, :, :] = res[0]
         transmitted_beam[column_index] = numpy.sum(data_container[(column_index, *direct_beam_ROI)])
+        if column_index % 1000 == 0:
+            print("Image: {}".format(column_index))
 
     with h5py.File(h5filename_out, 'w') as output_file:
         output_file.create_dataset('I_all', data=output_data)
-        # output_file.create_dataset('phi_det', data=phi)
-        # output_file.create_dataset('q', data=az.radia)
-        output_file.create_dataset('i_t', data=transmitted_beam)
-        output_file.create_dataset('n_pix', data=n_pix)
+        output_file.create_dataset('azi', data=az.azimuth_axis)
+        output_file.create_dataset('q', data=az.radial_axis)
+        output_file.create_dataset('I_t', data=transmitted_beam)
+        output_file.create_dataset('n_pix', data=az.norm.reshape(az.output_shape))
+        output_file.create_dataset('corr', data=az.corrections.reshape(az.output_shape))
+        output_file.create_dataset('mask', data=bad_pix_map)
