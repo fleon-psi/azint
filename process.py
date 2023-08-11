2179f3824d8a19299db3fe38f29cfd7a539d32bd
Filip Leonarski <filip.leonarski@psi.ch>
2023-08-11 15:35:45 +0200

    Commit 4

--- process.py
+++ process.py
@@ -10,11 +10,18 @@ ponifilename = sys.argv[2]
 n_bins_rad = 500
 n_bins_azi = 16
 pixel_split = 2
+pixel_size = 75.0e-6
 
 direct_beam_min_x = 1990
 direct_beam_max_x = 2015
 direct_beam_min_y = 2377
 direct_beam_max_y = 2389
+
+if len(sys.argv) >= 8:
+    direct_beam_min_x = int(sys.argv[4])
+    direct_beam_max_x = int(sys.argv[5])
+    direct_beam_min_y = int(sys.argv[6])
+    direct_beam_max_y = int(sys.argv[7])
 
 direct_beam_ROI = (slice(direct_beam_min_y, direct_beam_max_y),
                    slice(direct_beam_min_x, direct_beam_max_x))
@@ -31,7 +38,7 @@ with h5py.File(base_directory + h5filename_in, 'r') as input_file:
 
     image_size = (data_container.shape[1], data_container.shape[2])
 
-    if len(sys.argv) == 4:
+    if len(sys.argv) >= 4:
         with h5py.File(sys.argv[3], 'r') as file:
             bad_pix_map = numpy.array(file['data']).astype(bool)
     else:
@@ -39,7 +46,7 @@ with h5py.File(base_directory + h5filename_in, 'r') as input_file:
 
     az = azint.AzimuthalIntegrator(ponifilename,
                                    shape=image_size,
-                                   pixel_size=75.0e-06,
+                                   pixel_size=pixel_size,
                                    n_splitting=pixel_split,
                                    radial_bins=n_bins_rad,
                                    azimuth_bins=n_bins_azi,
@@ -49,22 +56,36 @@ with h5py.File(base_directory + h5filename_in, 'r') as input_file:
                                    error_model=None,
                                    polarization_factor=None)
 
+    az_no_corr = azint.AzimuthalIntegrator(ponifilename,
+                                           shape=image_size,
+                                           pixel_size=pixel_size,
+                                           n_splitting=pixel_split,
+                                           radial_bins=n_bins_rad,
+                                           azimuth_bins=n_bins_azi,
+                                           unit='q',
+                                           mask=bad_pix_map,
+                                           solid_angle=False,
+                                           error_model=None,
+                                           polarization_factor=None)
+
     for column_index in range(nimages):
         res = az.integrate(data_container[column_index, :, :])
         output_data[column_index, :, :] = res[0]
-        transmitted_beam[column_index] = numpy.sum(data_container[(column_index, *direct_beam_ROI)])
+        if (direct_beam_min_x < direct_beam_max_x) and (direct_beam_min_y < direct_beam_max_y):
+            transmitted_beam[column_index] = numpy.sum(data_container[(column_index, *direct_beam_ROI)])
         if column_index % 1000 == 0:
             print("Image: {}".format(column_index))
 
-    output_directory = os.path.basename(output_directory + h5filename_in)
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
+    output_dir_full = os.path.dirname(output_directory + h5filename_in)
+    if not os.path.exists(output_dir_full):
+        os.makedirs(output_dir_full)
 
     with h5py.File(output_directory + h5filename_in, 'w') as output_file:
         output_file.create_dataset('I_all', data=output_data)
         output_file.create_dataset('azi', data=az.azimuth_axis)
         output_file.create_dataset('q', data=az.radial_axis)
-        output_file.create_dataset('I_t', data=transmitted_beam)
-        output_file.create_dataset('n_pix', data=az.norm.reshape(az.output_shape))
+        if (direct_beam_min_x < direct_beam_max_x) and (direct_beam_min_y < direct_beam_max_y):
+            output_file.create_dataset('I_t', data=transmitted_beam)
+        output_file.create_dataset('n_pix', data=az_no_corr.norm.reshape(az_no_corr.output_shape))
         output_file.create_dataset('corr', data=az.corrections.reshape(az.output_shape))
         output_file.create_dataset('mask', data=bad_pix_map)
